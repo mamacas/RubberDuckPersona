02a5f67b1e160ea2d697b5b0a9e05ee12c3959ea
Brett Jay Packard <brettjayp@gmail.com>
2019-12-11 21:45:40 -0800

    building constructors

--- js/app.js
+++ js/app.js
@@ -1,1 +1,61 @@
 'use strict';
+
+// Global variables and document elements
+let userObjects = [];
+let quizObjects = [];
+let historyObjects = [];
+
+// Constructor functions
+function User(userName){
+  this.username = userName,
+  this.attempts = [],
+  userObjects.push(this);
+}
+
+function Quiz(title){
+  this.title = title,
+  this.quizCounter = 0,
+  this.questions = [],
+  this.results = [],
+  quizObjects.push(this);
+}
+
+// function History(quiz){
+//   this.quiz = quiz,
+//   this.cummulative = 0,
+//   this.resultCounter = [],
+//   historyObjects.push(this);
+// }
+
+// Prototype functions
+
+User.prototype.newAttempt = function(quiz, result){
+  this.attempts.push([quiz, result]);
+};
+
+Quiz.prototype.addQuestion = function(question, responseA, weightA, responseB, weightB){
+  this.results.push([question, responseA, weightA, responseB, weightB]);
+};
+
+Quiz.prototype.newResult = function(resultTitle, resultBio, imagePath, imageAtl, imageTitle){
+  let resultCounter = 0;
+  this.questions.push([resultTitle, resultCounter, resultBio, imagePath, imageAtl, imageTitle]);
+};
+
+Quiz.prototype.newAttempt = function(resultToIncrement){
+  ++this.quizCounter;
+  this.results.forEach(function(n)){
+    if(n[0] === resultToIncrement){
+      ++n[1];
+    }
+  }
+};
+
+// History.prototype.newResult = function(result){
+//   this.cummulative++;
+//   if ()
+// }
+
+// Helper functions
+
+
